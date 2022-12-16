76468fbfc7c07bbc165a74b5d2ea1b9a44591b66
victorgabriel <112019336+VictorGabriel01@users.noreply.github.com>
2022-12-15 22:02:05 -0300

    Add files via upload

---
+++ Exercicio javascript do while/js/script3.js
@@ -0,0 +1,7 @@
+var contador = 0;
+do {
+    if (contador % 4 == 0) {
+        alert(contador)
+    }
+    contador++;
+} while (contador == 200);
---
+++ Exercicio javascript do while/js/script5.js
@@ -0,0 +1,9 @@
+do {
+    var numero = parseInt(prompt("Digite um número:"));
+    contador++;
+    var fatorial = 1;
+    for (let contador = 0; contador < numero; contador++) {
+        var fatorial = fatorial*contador;
+        
+    }
+} while (contador == 15);
